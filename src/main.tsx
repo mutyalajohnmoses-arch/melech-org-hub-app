0779c250999568318f59375b1d93628922caad0e
mutyalajohnmoses-arch <mutyalajohnmoses@gmail.com>
2026-09-23 18:57:01 +0530

    Connect website control runtime

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { SiteControlProvider } from './lib/melech-site-control'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SiteControlProvider siteKey="melech-org-hub-app"><App /></SiteControlProvider>
   </React.StrictMode>,
 )
